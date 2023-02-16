d5f69475dcf77affc65dcdaeb760b7ae8689f96f
Antony Itki <59201310+antonyitki@users.noreply.github.com>
2023-02-16 19:46:48 +0000

    working, but need to be organized.

---
+++ test.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+with DAG(
+    "my-first-dag",
+    default_args={
+        "depends_on_past": False,
+        "email": ["tony@tony.com"],
+        "email_on_failure": False,
+        "email_on_rety": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+    },
+    description="A simple tutorial DAG",
+    schedule=timedelta(days=1),
+    start_date=datetime(2023, 2, 1),
+    catchup=False,
+    tags=["example"],
+) as dag:
+    t1 = BashOperator(
+        task_id="print_date",
+        bash_command="date"
+    )
+    t2 = BashOperator(
+        task_id="sleep",
+        depends_on_past=False,
+        bash_command="sleep 5",
+        retries=3,
+    )
+    templated_command = dedent(
+        """
+        
+        """
+    )
+    t3 = BashOperator(
+        task_id="templated",
+        depends_on_past=False,
+        bash_command=templated_command
+    )
+
+    t1 >> [t2, t3]
